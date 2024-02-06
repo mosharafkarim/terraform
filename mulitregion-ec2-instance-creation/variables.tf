6c03bf459544126f27fa152534d9bd17a7b933a0
mosharafkarim <69795428+mosharafkarim@users.noreply.github.com>
2024-02-06 21:31:27 +0530

    Update variables.tf

--- mulitregion-ec2-instance-creation/variables.tf
+++ mulitregion-ec2-instance-creation/variables.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 variable "aws_ec2_ami_nvirigina" {
     description = "Ami of ec2 instance"
     type=string
@@ -22,29 +21,4 @@ variable "aws_ec2_key" {
 variable "aws_ec2_sg" {
     description = "Security group of ec2 instance"
     type = list
-=======
-variable "aws_ec2_ami_nvirigina" {
-    description = "Ami of ec2 instance"
-    type=string
-}
-variable "aws_resource_name" {
-  description = "Name of the resource"
-  type = map
-}
-variable "aws_ec2_ami_ohio" {
-    description = "Ami of ec2 instance"
-    type = string
-}
-variable "aws_ec2_type" {
-    description = "Type of ec2 instance"
-    type = string
-}
-variable "aws_ec2_key" {
-    description = "Kaypair of ec2 instance"
-    type = string
-}
-variable "aws_ec2_sg" {
-    description = "Security group of ec2 instance"
-    type = list
->>>>>>> 6744de251eaf9e57741a3b576d826547151d5a3c
 }
