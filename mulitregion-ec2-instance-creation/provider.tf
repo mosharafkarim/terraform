e222f81b37d59826230a82b6548a5ecde52cdd5b
mosharafkarim <69795428+mosharafkarim@users.noreply.github.com>
2025-07-10 19:45:47 +0530

    Update provider.tf

--- mulitregion-ec2-instance-creation/provider.tf
+++ mulitregion-ec2-instance-creation/provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.6" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }        
+  }
+}
+
 provider "aws" {
     alias="us-east-1"
     region="us-east-1"
